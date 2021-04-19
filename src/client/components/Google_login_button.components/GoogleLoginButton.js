ebb73f2f23a76421d91bb6218f84dae1050acaba
TITHI1244 <tithi124410@gmail.com>
2021-04-19 14:12:47 +0200

    initial commit for adding Google login button

---
+++ src/client/components/Google_login_button.components/GoogleLoginButton.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './GoogleLoginButton.css';
+import GoogleLogo from '../../assets/images/Google_logo.png';
+
+const GoogleLoginButton = () => {
+  return (
+    <>
+      <button className="Google_login_button" type="submit">
+        <img
+          className="google_logo_img"
+          alt="Google_logo_for_login"
+          src={GoogleLogo}
+        />
+        <span>Sign up with Google</span>
+      </button>
+    </>
+  );
+};
+
+export default GoogleLoginButton;
---
+++ src/client/components/Google_login_button.components/GoogleLoginButton.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import GoogleLoginButton from './GoogleLoginButton';
+
+export default { title: 'GoogleLoginButton' };
+
+export function Login_with_Google() {
+  return <GoogleLoginButton />;
+}
