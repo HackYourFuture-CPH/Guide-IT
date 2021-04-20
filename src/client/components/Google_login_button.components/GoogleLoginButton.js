8aa11639908c571963ea96a5d6d4858b0c29c118
TITHI1244 <tithi124410@gmail.com>
2021-04-20 14:17:27 +0200

    added a control for changing the fonts of the button text

--- src/client/components/Google_login_button.components/GoogleLoginButton.js
+++ src/client/components/Google_login_button.components/GoogleLoginButton.js
@@ -2,19 +2,23 @@ import React from 'react';
 import './GoogleLoginButton.css';
 import GoogleLogo from '../../assets/images/Google_logo.png';
 
-const GoogleLoginButton = () => {
+const GoogleLoginButton = ({ font }) => {
   return (
     <>
-      <button className="google_login_button" type="submit">
-        <img
-          className="google_logo_img"
-          alt="Google_logo_for_login"
-          src={GoogleLogo}
-        />
+      <button
+        className="google_login_button"
+        type="submit"
+        style={{ fontFamily: `${font}` }}
+      >
+        <img className="google_logo_img" alt="Google logo" src={GoogleLogo} />
         <span>Sign up with Google</span>
       </button>
     </>
   );
+};
+
+GoogleLoginButton.defaultProps = {
+  font: 'Roboto',
 };
 
 export default GoogleLoginButton;
--- src/client/components/Google_login_button.components/GoogleLoginButton.stories.js
+++ src/client/components/Google_login_button.components/GoogleLoginButton.stories.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import GoogleLoginButton from './GoogleLoginButton';
 
-export default { title: 'GoogleLoginButton' };
+export default {
+  title: 'GoogleLoginButton',
+  component: GoogleLoginButton,
+  argTypes: {
+    font: {
+      control: { type: 'select', options: ['Roboto', 'Roboto mono'] },
+    },
+  },
+};
 
-export function loginWithGoogle() {
-  return <GoogleLoginButton />;
+export function loginWithGoogle({ font }) {
+  return <GoogleLoginButton font={font} />;
 }
+
+GoogleLoginButton.args = {
+  font: 'Roboto',
+};
+
+GoogleLoginButton.propTypes = {
+  font: PropTypes.string,
+};
