df4bae31f2bbd738cb6d24d209c3a661da4f8a29
aijazraja <bazaaraijaz@gmail.com>
2021-04-22 14:39:32 +0200

    Storybook problem solved

--- src/client/components/RobotLogo/RobotLogo.js
+++ src/client/components/RobotLogo/RobotLogo.js
@@ -4,10 +4,10 @@ import Robot from '../../assets/images/robot_logo.png';
 import Arrow from '../../assets/images/arrow.png';
 import './RobotLogo.css';
 
-export default function RobotLogo({ handleOnclick }) {
+export default function RobotLogo({ onClick }) {
   return (
     <div className="RobotLogo">
-      <button onClick={handleOnclick} type="submit" className="RobotLogoDiv">
+      <button onClick={onClick} type="submit" className="RobotLogoDiv">
         <img src={Robot} alt="RobotLogo" />
         <p>
           CAREER FOR <br />
@@ -21,9 +21,9 @@ export default function RobotLogo({ handleOnclick }) {
   );
 }
 RobotLogo.propTypes = {
-  handleOnclick: PropTypes.func,
+  onClick: PropTypes.func,
 };
 
 RobotLogo.defaultProps = {
-  handleOnclick: null,
+  onClick: null,
 };
--- src/client/components/RobotLogo/RobotLogo.stories.js
+++ src/client/components/RobotLogo/RobotLogo.stories.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { action } from '@storybook/addon-actions';
 
 import RobotLogo from './RobotLogo';
 
 export default {
   title: 'Components / Robot Component',
   component: RobotLogo,
+  argTypes: {
+    onClick: { action: 'clicked' },
+  },
+  parameters: { actions: { argTypesRegex: '^on.*' } },
 };
 
-export const Component = () => <RobotLogo />;
+export const Component = () => (
+  <RobotLogo onClick={action('You have clicked the button')} />
+);
