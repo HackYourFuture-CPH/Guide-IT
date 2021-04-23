37997430ed26b8e1d8807546fdfd6ce8391e8665
rajanibusani <brajanipharma@gmaill.com>
2021-04-23 13:06:14 +0200

    added a extra line after import

--- src/client/components/EmailRegistration/EmailForm.js
+++ src/client/components/EmailRegistration/EmailForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './EmailForm.css';
+
 export default function EmailRegistration({ formWidth, fontFamily }) {
   const [email, setEmail] = useState('');
   const [error, setError] = useState('');
@@ -14,7 +15,7 @@ export default function EmailRegistration({ formWidth, fontFamily }) {
     const pattern = new RegExp(
       /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i,
     );
-    // if enter key is pressed, validating user email.
+    //  if enter key is pressed, validating user email.
     if (e.key === 'Enter') {
       e.preventDefault();
       if (email === '') {
