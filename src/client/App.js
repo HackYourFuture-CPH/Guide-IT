47c9de8a8e096b726ae8109d3efbdaf61540e43c
Abdulrahman Mohamad Aarabi <abma@moe.dk>
2021-04-23 08:45:32 +0200

    rename css files to DescriptionCareer.css

--- src/client/App.js
+++ src/client/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import { Home } from './containers/Home/Home';
 import SignIn from './containers/SignIn';
 import SignUp from './containers/SignUp';
 import ResetPassword from './containers/ResetPassword';
--- src/client/components/DescriptionCareer/DescriptionCareer.js
+++ src/client/components/DescriptionCareer/DescriptionCareer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './descriptioncareer.css';
+import './DescriptionCareer.css';
 
 function DescriptionCareer({ title, content, content2, time }) {
   return (
