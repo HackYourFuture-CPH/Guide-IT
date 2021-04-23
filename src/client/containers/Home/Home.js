cc8bfa62374e8055e6163759cbc405f19f5754e4
Abdulrahman Mohamad Aarabi <abma@moe.dk>
2021-04-23 08:25:41 +0200

    remove Home folder

--- src/client/containers/Home/Home.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const Home = () => (
-  <div>
-    <section className="home">Final project!</section>
-    return <div>This is Home.component.js</div>;
-  </div>
-);
