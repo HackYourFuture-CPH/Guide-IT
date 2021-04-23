0853de7d87fc8625fe71e3ede139025378da68b1
Beth Jackson <beth.jack@gmail.com>
2021-04-23 12:22:18 +0200

    Removed answer part from component

--- src/client/components/Question/QuestionComponent.js
+++ src/client/components/Question/QuestionComponent.js
@@ -2,74 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './QuestionComponent.styles.css';
 
-export default function QuestionComponent({
-  question,
-  imageSrc,
-  options,
-  selectedOption,
-  onOptionChange,
-  children,
-}) {
+export default function QuestionComponent({ question, imageSrc }) {
   return (
-    <div className="question-component">
-      <div
-        className="question-box"
-        style={{ backgroundImage: `url(${imageSrc})` }}
-      >
-        <p className="question">{question}</p>
-      </div>
-      {/* These next lines are the logic that controlls whether answer options are listed (multichoice) */}
-      {/* If no options are given, it assumes an agree scale for the answer */}
-      {options === null ? (
-        <div className="question-options-agree-scale">
-          <label className="question-option-label">
-            Disagree
-            <input
-              type="radio"
-              name="answer"
-              checked={selectedOption === 0}
-              onChange={() => onOptionChange(0)}
-            />
-          </label>
-          <label className="question-option-label">
-            <input
-              type="radio"
-              name="answer"
-              checked={selectedOption === 1}
-              onChange={() => onOptionChange(1)}
-            />
-          </label>
-          <label className="question-option-label">
-            <input
-              type="radio"
-              name="answer"
-              checked={selectedOption === 2}
-              onChange={() => onOptionChange(2)}
-            />
-            Agree
-          </label>
-        </div>
-      ) : (
-        <div className="question-options-multi-choice">
-          {options.map((option, i) => {
-            return (
-              <label className="question-option-label">
-                <input
-                  type="radio"
-                  name="answer"
-                  checked={selectedOption === i}
-                  onChange={() => onOptionChange(i)}
-                />
-                {option}
-              </label>
-            );
-          })}
-        </div>
-      )}
-      {/* Need to add back and next buttons in here as children once they have been created. */}
-      {/* Q1 will not have a back button */}
-      {/* Last question will not have a next button */}
-      {children}
+    <div
+      className="question-box"
+      style={{ backgroundImage: `url(${imageSrc})` }}
+    >
+      <p className="question">{question}</p>
     </div>
   );
 }
@@ -77,14 +16,4 @@ export default function QuestionComponent({
 QuestionComponent.propTypes = {
   question: PropTypes.string.isRequired,
   imageSrc: PropTypes.string.isRequired,
-  options: PropTypes.arrayOf(PropTypes.string),
-  selectedOption: PropTypes.number,
-  onOptionChange: PropTypes.func,
-  children: PropTypes.element.isRequired,
-};
-
-QuestionComponent.defaultProps = {
-  options: null,
-  selectedOption: null,
-  onOptionChange: null,
 };
--- src/client/components/Question/QuestionComponent.stories.js
+++ src/client/components/Question/QuestionComponent.stories.js
@@ -1,7 +1,6 @@
 import { array, number, text } from '@storybook/addon-knobs';
 import React from 'react';
 import image1 from '../../assets/images/questionBackgrounds/question1background.png';
-import image2 from '../../assets/images/questionBackgrounds/question2background.png';
 
 import QuestionComponent from './QuestionComponent';
 
@@ -17,22 +16,5 @@ export const MultiChoice = () => (
       'When visiting a website, what is it that you are most interested in?',
     )}
     imageSrc={text('imageSrc', image1)}
-    options={array('Options', [
-      'How the website looks and how easy it is for the users',
-      'Logic behind how the website is built',
-    ])}
-    selectedOption={number('Selected Option', 0)}
-  />
-);
-
-export const AgreeScale = () => (
-  <QuestionComponent
-    question={text(
-      'Question',
-      'I can easily notice changes around me including people, culture, trends, etc.',
-    )}
-    imageSrc={text('imageSrc', image2)}
-    options={array('Options', null)}
-    selectedOption={number('Selected Option', 0)}
   />
 );
