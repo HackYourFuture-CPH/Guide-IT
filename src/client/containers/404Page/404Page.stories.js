b261ea50fd59617fdc3c067e60698701b414721f
sandhana14 <sandhana14@gmail.com>
2021-04-27 21:58:08 +0200

    Added error 404 page component

---
+++ src/client/containers/404Page/404Page.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Error404Page from './404Page.component';
+
+export default {
+  title: 'containers / Error 404 Page ',
+  component: Error404Page,
+};
+
+export const Errorpage = () => <Error404Page />;
