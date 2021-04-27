25e0e8e1770faf3e1560f9959aba98feded7303a
ccshah298 <ccshah298@gmail.com>
2021-04-27 23:47:28 +0200

    quiz page ready

--- src/client/containers/QuizPage/QuizPage.component.js
+++ src/client/containers/QuizPage/QuizPage.component.js
@@ -1,3 +1,41 @@
 import React from 'react';
-
-export const QuizPage = () => <section>Quiz Page</section>;
+import './QuizPage.styles.css';
+import PageHeader from '../../components/PageHeaderComponent/PageHeader';
+import QuestionComponent from '../../components/Question/QuestionComponent';
+import QuizAnswers from '../../components/QuizAnswers/QuizAnswers';
+import SideMenu from '../../components/sideMenubar/sideMenu';
+import Buttons from '../../components/Buttons/Buttons';
+import ProgressBar from '../../components/ProgressBar.components/ProgressBar';
+import image1 from '../../assets/images/questionBackgrounds/question1background.png';
+export const QuizPage = () => {
+  return (
+    <>
+      <div>
+        <PageHeader />
+      </div>
+      <div className="middle_page">
+        <div className="sidemenu_page">
+          <SideMenu />
+        </div>
+        <div className="question_page">
+          <QuestionComponent
+            imageSrc={image1}
+            question="When visiting a website, what is that you are most interested in?"
+          />
+          <div className="quizans_page">
+            <QuizAnswers
+              firstAnswer="How the website looks and how easy it is for the users"
+              secondAnswer="Logic behind how the website is built"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="button_page">
+        <Buttons label="Next" size="big" isMono="false" />
+      </div>
+      <div className="progressbar_page">
+        <ProgressBar />
+      </div>
+    </>
+  );
+};
