10d472b3151f78581c2358c7055a629dec1cc080
sofiiadidovych <sofiiadidovych@gmail.com>
2021-05-02 13:10:46 +0200

    Add fk to quiz_results table

--- src/server/migrations/20210502125651_create_quiz_results.js
+++ src/server/migrations/20210502125651_create_quiz_results.js
@@ -4,7 +4,9 @@ exports.up = function (knex) {
   return knex.schema.createTable('quiz_results', (table) => {
     table.increments();
     table.integer('fk_answer_id').unsigned().notNullable();
+    table.foreign('fk_answer_id').references('answers.id');
     table.integer('fk_user_id').unsigned().notNullable();
+    table.foreign('fk_user_id').references('users.id');
     table.datetime('created_date').defaultTo(knex.fn.now()).notNullable();
   });
 };
@@ -12,5 +14,3 @@ exports.up = function (knex) {
 exports.down = function (knex) {
   return knex.schema.dropTable('quiz_results');
 };
-
-// TODO: Add foreign keys when users and answers tables will be created
