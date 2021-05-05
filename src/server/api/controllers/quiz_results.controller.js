35569b30a11faa8cee5bf34f40521e63c2348cab
TITHI1244 <tithi124410@gmail.com>
2021-05-05 22:00:02 +0200

    removed one commented out line of code

--- src/server/api/controllers/quiz_results.controller.js
+++ src/server/api/controllers/quiz_results.controller.js
@@ -2,7 +2,6 @@ const knex = require('../../config/db');
 
 const createQuizResults = async (body) => {
   await knex('quiz_results').insert({
-    // id: body.id,
     fk_answer_id: body.fk_answer_id,
     fk_user_id: body.fk_user_id,
   });
