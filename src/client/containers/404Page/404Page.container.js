3e93d1b927fdf75603878f80493cc3d2ce575606
sandhana14 <sandhana14@gmail.com>
2021-05-06 13:55:24 +0200

    Changed some css for resposiveness and added link to the button

--- src/client/containers/404Page/404Page.container.js
+++ src/client/containers/404Page/404Page.container.js
@@ -3,12 +3,13 @@ import './404Page.styles.css';
 import cable from '../../assets/images/cable.png';
 import sadrobot from '../../assets/images/sadrobot.png';
 import Buttons from '../../components/Buttons/Buttons';
+import { Link } from 'react-router-dom';
 
 function Error404Page() {
   return (
     <div className="body-background-image">
       <div className="center-container">
-        <img src={cable} alt="cable" />
+        <img src={cable} className="cable" alt="cable" />
 
         <div className="main-text">
           <h1>404</h1>
@@ -21,7 +22,9 @@ function Error404Page() {
         </div>
 
         <div>
-          <Buttons label="Go Home" size="big" isMono={true} />
+          <Link to="/">
+            <Buttons label="Go Home" size="big" isMono={true} />
+          </Link>
         </div>
       </div>
     </div>
