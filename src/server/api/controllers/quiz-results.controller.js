eb9a169d7ca4fb112cba97d479f728b94e8019e9
qamarfj <qamarfj@hotmail.com>
2021-05-12 17:22:48 +0200

    added comments

--- src/server/api/controllers/quiz-results.controller.js
+++ src/server/api/controllers/quiz-results.controller.js
@@ -24,12 +24,15 @@ const getQuizResults = async (answerId, userId) => {
         .where('fk_answer_id', '=', answerId)
         .select('id', 'fk_answer_id', 'fk_user_id', 'created_date');
     } else if (userId !== undefined) {
-      const max = (await knex('questions').count())[0]['count(*)'];
-
+      // get total number of questions from questions table.
+      const numberOfQuestions = (await knex('questions').count())[0][
+        'count(*)'
+      ];
+      // Get latest records with userid from quiz_results upto to numberOfQuestions
       filteredResults = await knex('quiz_results')
         .where('fk_user_id', '=', userId)
         .orderBy('created_date', 'desc')
-        .limit(Number(max));
+        .limit(Number(numberOfQuestions));
     } else {
       filteredResults = await knex('quiz_results').select(
         'id',
