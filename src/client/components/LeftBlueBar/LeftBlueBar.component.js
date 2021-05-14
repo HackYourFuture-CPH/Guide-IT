0ac9dbb329434c5f67e25175c23e223efae9fa31
JyothiNandyala <njyothi451@gmail.com>
2021-05-14 22:44:53 +0200

    added the files for burger menu

--- src/client/components/LeftBlueBar/LeftBlueBar.component.js
+++ src/client/components/LeftBlueBar/LeftBlueBar.component.js
@@ -6,6 +6,7 @@ import rediLogo from '../../assets/images/redi-logo.png';
 function SidebarMenu() {
   return (
     <div className="sidebar-menu">
+      <input id="nav-toggle" type="checkbox" />
       <div className="sidebar-header">
         <div className="sidebar-title">GuideIT</div>
         <div className="create-account">
@@ -22,6 +23,11 @@ function SidebarMenu() {
           <div className="triangle" />
         </div>
       </div>
+      <label htmlFor="nav-toggle" className="icon-burger">
+        <div className="line" />
+        <div className="line" />
+        <div className="line" />
+      </label>
       <div className="sidebar-footer">
         <img src={hyfLogo} alt="HYF-logo" className="hyf-logo" />
         <img src={rediLogo} alt="REDI-logo" className="redi-logo" />
