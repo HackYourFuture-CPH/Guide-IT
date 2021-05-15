8c9dcd5e2dfc2f2284cd34662f3a403a4462a1fb
DlnyaMazhari <mazharidlnya@gmail.com>
2021-05-15 23:53:36 +0200

    files are added

--- src/client/components/Pagination/Pagination.component.js
+++ src/client/components/Pagination/Pagination.component.js
@@ -12,40 +12,41 @@ function Pagination({ num }) {
 
   const arr = [level0, level1, level2, level3, level4, level5];
 
-  const handleClick = (num) => {
+  const handleClick = (number) => {
     arr.forEach((item) => {
       if (item.current.style.border) {
+        /* eslint no-param-reassign: "error" */
         item.current.style.border = 'none';
       }
     });
-    setFloor(num);
+    setFloor(number);
   };
 
   useEffect(() => {
-    if (floor != undefined) {
+    if (floor !== undefined) {
       arr[floor].current.style.border = '1px solid #F0F0F0';
     }
-  }, [floor]);
+  }, [floor, arr]);
 
   return (
     <div className="pagination-box">
       <div className="pagination-numbers">
-        <button ref={level0} onClick={() => handleClick(0)}>
+        <button type="button" ref={level0} onClick={() => handleClick(0)}>
           0
         </button>
-        <button ref={level1} onClick={() => handleClick(1)}>
+        <button type="button" ref={level1} onClick={() => handleClick(1)}>
           1
         </button>
-        <button ref={level2} onClick={() => handleClick(2)}>
+        <button type="button" ref={level2} onClick={() => handleClick(2)}>
           2
         </button>
-        <button ref={level3} onClick={() => handleClick(3)}>
+        <button type="button" ref={level3} onClick={() => handleClick(3)}>
           3
         </button>
-        <button ref={level4} onClick={() => handleClick(4)}>
+        <button type="button" ref={level4} onClick={() => handleClick(4)}>
           4
         </button>
-        <button ref={level5} onClick={() => handleClick(5)}>
+        <button type="button" ref={level5} onClick={() => handleClick(5)}>
           5
         </button>
       </div>
