b415c8f4313fdcb39a6830f888978fb205294203
DlnyaMazhari <mazharidlnya@gmail.com>
2021-05-18 21:17:32 +0200

    Pagination was done by using useRef, now it is changed and created by useState

--- src/client/components/Pagination/Pagination.component.js
+++ src/client/components/Pagination/Pagination.component.js
@@ -1,55 +1,44 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Pagination.styles.css';
 
 function Pagination({ num }) {
   const [floor, setFloor] = useState(num);
-  const level0 = useRef();
-  const level1 = useRef();
-  const level2 = useRef();
-  const level3 = useRef();
-  const level4 = useRef();
-  const level5 = useRef();
+  const [elements, setElements] = useState();
 
-  const arr = [level0, level1, level2, level3, level4, level5];
+  const floorArray = [0, 1, 2, 3, 4, 5];
 
-  const handleClick = (number) => {
-    arr.forEach((item) => {
-      if (item.current.style.border) {
-        /* eslint no-param-reassign: "error" */
-        item.current.style.border = 'none';
-      }
+  const renderFloors = () => {
+    return floorArray.map((item) => {
+      return item === floor ? (
+        <button
+          type="button"
+          key={item}
+          className="chosen-floor"
+          onClick={() => setFloor(item)}
+        >
+          {item}
+        </button>
+      ) : (
+        <button
+          type="button"
+          key={item}
+          className="unchosen-floor"
+          onClick={() => setFloor(item)}
+        >
+          {item}
+        </button>
+      );
     });
-    setFloor(number);
   };
 
   useEffect(() => {
-    if (floor !== undefined) {
-      arr[floor].current.style.border = '1px solid #F0F0F0';
-    }
-  }, [floor, arr]);
+    setElements(renderFloors);
+    // eslint-disable-next-line
+  }, [floor]);
 
   return (
     <div className="pagination-box">
-      <div className="pagination-numbers">
-        <button type="button" ref={level0} onClick={() => handleClick(0)}>
-          0
-        </button>
-        <button type="button" ref={level1} onClick={() => handleClick(1)}>
-          1
-        </button>
-        <button type="button" ref={level2} onClick={() => handleClick(2)}>
-          2
-        </button>
-        <button type="button" ref={level3} onClick={() => handleClick(3)}>
-          3
-        </button>
-        <button type="button" ref={level4} onClick={() => handleClick(4)}>
-          4
-        </button>
-        <button type="button" ref={level5} onClick={() => handleClick(5)}>
-          5
-        </button>
-      </div>
+      <div className="pagination-numbers">{elements}</div>
     </div>
   );
 }
