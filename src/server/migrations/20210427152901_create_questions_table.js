546013454aee0f55333cd8ebe71912ef554404fb
qamarfj <qamarfj@hotmail.com>
2021-05-20 21:46:14 +0200

    added two columns in questions table

--- src/server/migrations/20210427152901_create_questions_table.js
+++ src/server/migrations/20210427152901_create_questions_table.js
@@ -3,6 +3,8 @@ exports.up = function (knex) {
   return knex.schema.createTable('questions', (table) => {
     table.increments();
     table.string('question').notNullable();
+    table.string('image_url');
+    table.bool(' is_agreement_question');
     table.datetime('created_date').defaultTo(knex.fn.now()).notNullable();
   });
 };
