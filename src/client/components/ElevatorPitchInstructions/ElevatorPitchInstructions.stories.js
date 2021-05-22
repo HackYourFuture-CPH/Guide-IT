b7cf564f556c8e74dba2f0b9f41be9437e2e9816
rajanibusani <brajanipharma@gmaill.com>
2021-05-22 15:17:09 +0200

    added oclick in elevator instructions

--- src/client/components/ElevatorPitchInstructions/ElevatorPitchInstructions.stories.js
+++ src/client/components/ElevatorPitchInstructions/ElevatorPitchInstructions.stories.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ElevatorPitchInstructions from './ElevatorPitchInstructions.component';
+import PropTypes from 'prop-types';
 
 export default {
   title: 'components / Elevator pitch instructions ',
   component: ElevatorPitchInstructions,
 };
 
-export const Errorpage = () => <ElevatorPitchInstructions />;
+export const ElevatorInstructions = ({ onClick }) => (
+  <ElevatorPitchInstructions onClick={onClick} />
+);
+
+ElevatorInstructions.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
